f415b0ac87b60a1e9a93d3b22d1936d2f3669b2a
pranavmali-create <pranavmali0107@gmail.com>
2026-05-07 14:43:56 +0530

    feat(color): add kanagawa theme support

--- Session.vim
+++ Session.vim
@@ -1,20 +1,20 @@
 let SessionLoad = 1
 let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
 let v:this_session=expand("<sfile>:p")
+doautoall SessionLoadPre
 silent only
 silent tabonly
-cd ~/.config/bashnv
+cd ~/.config/nvim
 if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
 let s:shortmess_save = &shortmess
 set shortmess+=aoO
-badd +0 plugin/options.lua
+badd +21 plugin/keymaps.lua
 argglobal
 %argdel
-edit plugin/options.lua
+edit plugin/keymaps.lua
 argglobal
-balt plugin/options.lua
 setlocal foldmethod=manual
 setlocal foldexpr=v:lua.vim.treesitter.foldexpr()
 setlocal foldmarker={{{,}}}
@@ -25,12 +25,12 @@ setlocal foldnestmax=20
 setlocal foldenable
 silent! normal! zE
 let &fdl = &fdl
-let s:l = 1 - ((0 * winheight(0) + 22) / 45)
+let s:l = 21 - ((20 * winheight(0) + 17) / 35)
 if s:l < 1 | let s:l = 1 | endif
 keepjumps exe s:l
 normal! zt
-keepjumps 1
-normal! 07|
+keepjumps 21
+normal! 026|
 tabnext 1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
   silent exe 'bwipe ' . s:wipebuf
@@ -43,6 +43,7 @@ if filereadable(s:sx)
   exe "source " . fnameescape(s:sx)
 endif
 let &g:so = s:so_save | let &g:siso = s:siso_save
+nohlsearch
 doautoall SessionLoadPost
 unlet SessionLoad
 " vim: set ft=vim :
--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -4,30 +4,37 @@ local augroup = vim.api.nvim_create_augroup
 -- vim.cmd([[set noswapfile]])
 
 autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({
-            timeout = 40,
-        })
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      timeout = 40,
+    })
+  end,
 })
 
 autocmd('LspAttach', {
-    group = augroup('LSP actions',{}),
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>ci", function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[j", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "[k", function() vim.diagnostic.goto_prev() end, opts)
-    end
+  group = augroup('LSP actions', {}),
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ci", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[j", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "[k", function() vim.diagnostic.goto_prev() end, opts)
+  end
 })
 
+autocmd("ColorScheme", {
+  desc = "Show ~ on empty lines",
+  group = augroup("show-eob", { clear = true }),
+  callback = function()
+    vim.opt.fillchars = vim.opt.fillchars + { eob = "~" }
+  end,
+})
